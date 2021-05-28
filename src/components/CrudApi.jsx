603951ee250fbf7505f06ef97bb7f968e949aa8d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-05-27 22:38:57 -0500

    Clase 34 terminada

--- src/components/CrudApi.jsx
+++ src/components/CrudApi.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
 
-import CrudForm from './CrudForm'
-import CrudTable from './CrudTable'
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
 
 const initialDb = [
 	{
@@ -29,34 +30,48 @@ const initialDb = [
 		name: 'Ikki',
 		constellation: 'Fenix',
 	},
-]
+];
 
 const CrudApi = () => {
-	const [db, setDb] = useState([])
-	const [dataToEdit, setDataToEdit] = useState(null)
+	const [db, setDb] = useState([]);
+	const [dataToEdit, setDataToEdit] = useState(null);
+
+	let api = helpHttp();
+	let url = 'http://localhost:3333/santos';
+
+	useEffect(() => {
+		api.get(url).then(res => {
+			if (!res.err) {
+				setDb(res);
+			} else {
+				setDb([]);
+			}
+		});
+	}, []);
 
 	const createData = data => {
-		data.id = Date.now()
-		setDb([...db, data])
-	}
+		data.id = Date.now();
+		setDb([...db, data]);
+	};
 	const updateData = data => {
-		let newData = db.map(el => (el.id === data.id ? data : el))
-		setDb(newData)
-	}
+		let newData = db.map(el => (el.id === data.id ? data : el));
+		setDb(newData);
+	};
 	const deleteData = id => {
 		let isDelete = window.confirm(
 			`¿Estás seguro de eliminar el registro con el id? ${id}`,
-		)
+		);
 		if (isDelete) {
-			let newData = db.filter(el => el.id !== id)
-			setDb(newData)
+			let newData = db.filter(el => el.id !== id);
+			setDb(newData);
 		} else {
-			return
+			return;
 		}
-	}
+	};
 
 	return (
 		<div>
+			<h2>CRUD API</h2>
 			<h3>{dataToEdit ? 'Editar' : 'Agregar'}</h3>
 			<CrudForm
 				createData={createData}
@@ -70,7 +85,7 @@ const CrudApi = () => {
 				deleteData={deleteData}
 			/>
 		</div>
-	)
-}
+	);
+};
 
-export default CrudApi
+export default CrudApi;
