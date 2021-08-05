c421a4f457d536c2939a66c14b60493f1ff3499d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-05 10:05:56 -0500

    Empezando la clase 48

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -15,9 +15,31 @@ const styles = {
 
 const validationsForm = form => {
 	let errors = {};
+	const regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
+		regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/,
+		regexComments = /^.{1,255}$/;
 
 	if (!form.name.trim()) {
 		errors.name = "El campo 'Nombre' es requerido.";
+	} else if (!regexName.test(form.name.trim())) {
+		errors.name = "El campo 'Nombre' sóo acepta letras y espacios en blanco";
+	}
+
+	if (!form.email.trim()) {
+		errors.email = "El campo 'Email' es requerido.";
+	} else if (!regexEmail.test(form.email.trim())) {
+		errors.email = "El campo 'Email' es incorrecto";
+	}
+
+	if (!form.subject.trim()) {
+		errors.subject = "El campo 'Asunto' es requerido.";
+	}
+
+	if (!form.comments.trim()) {
+		errors.comments = "El campo 'Comentarios' es requerido.";
+	} else if (!regexComments.test(form.comments.trim())) {
+		errors.comments =
+			"El campo 'Comentarios' no debe exceder los 255 caracteres";
 	}
 
 	return errors;
