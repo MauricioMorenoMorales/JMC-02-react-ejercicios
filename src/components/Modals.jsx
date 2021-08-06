96e2d423143a0973dd3edb06cf1f5dfb622ae25a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-06 08:32:49 -0500

    Empezando la clase 52

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useModal } from '../hooks/useModal';
 import Modal from './Modal';
+import ContactForm from './ContactForm';
 
 const Modals = () => {
-	const [isOpenModal1, openModal1, closeModal1 ] = useModal(false)
-	const [isOpenModal2, openModal2, closeModal2 ] = useModal(false)
+	const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+	const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+	const [isOpenModal3, openModal3, closeModal3] = useModal(false);
 	return (
 		<div>
 			<h2>Modales</h2>
-			<button>Modal 1</button>
-			<Modal>
+			<button onClick={openModal1}>Modal 1</button>
+			<Modal isOpen={isOpenModal1} closeModal={closeModal1}>
 				<h3>Modal numero 1</h3>
 				<p>Hola este es el contenido de mi modal 1</p>
 				<img src="https://placeimg.com/400/400/animals" alt="animals" />
 			</Modal>
-			<button>Modal 2</button>
-			{/* <Modal>
+			<button onClick={openModal2}>Modal 2</button>
+			<Modal isOpen={isOpenModal2} closeModal={closeModal2}>
 				<h3>Otro Modal</h3>
 				<p>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
@@ -24,7 +26,11 @@ const Modals = () => {
 					cupiditate, architecto quos maiores.
 				</p>
 				<img src="https://placeimg.com/400/400/nature" alt="Nature" />
-			</Modal> */}
+			</Modal>
+			<button onClick={openModal3}>Modal 3</button>
+			<Modal isOpen={isOpenModal3} closeModal={closeModal3}>
+				<ContactForm />
+			</Modal>
 		</div>
 	);
 };
